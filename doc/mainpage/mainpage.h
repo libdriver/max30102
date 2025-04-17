62292cb4fc95d863d98b6958eaca31da7c2072cc
lishifeng <lishifenging@outlook.com>
2025-04-18 00:26:40 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver MAX30102
- * @brief     LibDriver MAX30102 is a full function driver of MAX30102 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver MAX30102 is a full-featured driver of MAX30102 and it can run in both MCU and Linux platform.
  * @version   1.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2021/11/13  <td>1.0      <td>Shifeng Li  <td>first upload
